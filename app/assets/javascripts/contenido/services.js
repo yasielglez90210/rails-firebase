7ec78992a819b4f97ce780046b73e0647e42fc85
yasielelreyglez <yasielelreyglez@gmail.com>
2017-12-18 00:20:22 -0500

    eliminar service

--- app/assets/javascripts/contenido/services.js
+++ app/assets/javascripts/contenido/services.js
@@ -5,8 +5,13 @@ var $id = 1;
 var services = firebase.database().ref().child('services').set(null);
 var database = firebase.database().ref().child('services/service'+$id); 
 var $form = $('#formServices')[0];
+var $serviceKey = [];
 var datatable = $('#services').DataTable( {
         data: [],
+        "bSort" : false,
+        "searching": false,
+        "paging": false,
+        "sDom": 'rt',
         columns: [
             { title: "Nombre" },
             { title: "Descipción" },
@@ -17,7 +22,7 @@ var datatable = $('#services').DataTable( {
         "columnDefs": [ {
             "targets": -1,
             "data": null,
-            "defaultContent": '<button type="button" class="btn btn-danger btn-sm"><i class="fa fa-trash-o" aria-hidden="true"></i></button>'
+            "defaultContent": '<button onClick="deleteService(this)" type="button" class="btn btn-danger btn-sm"><i class="fa fa-trash-o" aria-hidden="true"></i></button>'
         } ]
     } );
 
@@ -30,15 +35,20 @@ $(document).ready(function () {
         var $description = $('#description').val();
         var $email = $('#email').val();
 
-      addServices($name, $description, $email);   
+      addService($name, $description, $email);   
     });
 });
 
-function addServices(name, description, email) {
+function addService(name, description, email) {
   database.set({name: name, description: description, email: email});
   $id = $id + 1;
   $form.reset();
   database = firebase.database().ref().child('services/service'+$id); 
+}
+
+function deleteService(button) {
+  var id = $(button).attr("data-delete");
+  var service = firebase.database().ref().child('services/'+id).remove();   
 }
 
 function allServices(){
@@ -48,15 +58,23 @@ function allServices(){
     datatable.clear();
     datatable.rows.add(dataset);
     datatable.draw();
-  });
+    
+    var buttons = $('#services button');
+    buttons.each(function(index, button){
+      $(button).attr("data-delete", $serviceKey[index]);
+    });
+  });  
 }
 
 function snapshotToArray(snapshot) {
     var returnArr = [];
+    $serviceKey = [];
     snapshot.forEach(function(childSnapshot) {
         var item = childSnapshot.val();
         item.key = childSnapshot.key;
-        var temp = [item.name, item.description, item.email, childSnapshot.key];
+        $serviceKey.push(item.key);
+        console.log($serviceKey);
+        var temp = [item.name, item.description, item.email];
         returnArr.push(temp);
     });
 
